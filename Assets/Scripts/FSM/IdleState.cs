160b4abd7c57cedc4859e2c7127fc608790f04bd
Ryzen\William <super_willl@live.ca>
2024-02-16 17:20:44 -0500

    * Pushed base Upgrade FSM

---
+++ Assets/Scripts/FSM/IdleState.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+
+public class IdleState : CharacterState
+{
+
+
+    public override void OnEnter()
+    {
+
+        Debug.Log("Enter state: FreeState\n");
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+
+    public override void OnFixedUpdate()
+    {
+
+    }
+
+    public override void OnExit()
+    {
+
+    }
+
+    public override bool CanEnter(IState currentState)
+    {
+	
+        return !m_stateMachine.PlayerIsNear();
+
+    }
+
+    public override bool CanExit()
+    {
+        return m_stateMachine.PlayerIsNear();
+    }
+
+}
